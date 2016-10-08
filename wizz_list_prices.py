363dd6caa7d367b4ef580fd5ef7a5fff7ba4d355
Attila Sasvari <asasvari@cloudera.com>
2016-10-09 00:20:41 +0200

    Update wizz_list_prices.py

--- wizz_list_prices.py
+++ wizz_list_prices.py
@@ -7,8 +7,8 @@ sys.argv.pop(0)
 def printFlights( flight_type ):
   for f in doc[flight_type]:  
     if f['price'] is None:
-      continue		
-    print(f['departureStation'] + " " + f['date'] + " " +str(f['price']['amount']))
+      continue
+    print(f['departureStation'] + " " + f['arrivalStation'] + " " + f['date'] + " " +str(f['price']['amount']))
 
 for arg in sys.argv:
   j=open(arg).read()
