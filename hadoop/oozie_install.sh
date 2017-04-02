ad367ed2a14ca24176937926328227c92d6d894f
attila-s <at.sasvari@gmail.com>
2017-04-02 17:39:47 +0200

    Update oozie_install.sh

--- hadoop/oozie_install.sh
+++ hadoop/oozie_install.sh
@@ -8,3 +8,12 @@ git clone https://github.com/apache/oozie.git
 . /etc/environment 
 cd oozie
 ./bin/mkdistro.sh -Puber -DskipTests -Dhadoop.version=2.6.0
+cd /oozie/distro/target/oozie-4.4.0-SNAPSHOT-distro/oozie-4.4.0-SNAPSHOT
+tar xf oozie-examples.tar.gz
+tar xf oozie-sharelib-4.4.0-SNAPSHOT.tar.gz
+yes | cp /hadoop-2.6.0/etc/hadoop/core-site.xml conf/hadoop-conf/
+bin/oozie-setup.sh sharelib create -fs hdfs://localhost:9000 -locallib share -concurrency 4
+/hadoop-2.6.0/bin/hdfs dfs -put examples/
+bin/oozied.sh start
+JOB_ID=$(bin/oozie job -oozie http://localhost:11000/oozie -config examples/apps/shell/job.properties -run -DnameNode=hdfs://localhost:9000 -DjobTracker=localhost:8032)
+
