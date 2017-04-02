7170913975b7938f6c8f9a737d783fa4eccc7b48
attila-s <at.sasvari@gmail.com>
2017-04-02 17:57:32 +0200

    Update oozie_install.sh

--- hadoop/oozie_install.sh
+++ hadoop/oozie_install.sh
@@ -15,5 +15,6 @@ yes | cp /hadoop-2.6.0/etc/hadoop/core-site.xml conf/hadoop-conf/
 bin/oozie-setup.sh sharelib create -fs hdfs://localhost:9000 -locallib share -concurrency 4
 /hadoop-2.6.0/bin/hdfs dfs -put examples/
 bin/oozied.sh start
-JOB_ID=$(bin/oozie job -oozie http://localhost:11000/oozie -config examples/apps/shell/job.properties -run -DnameNode=hdfs://localhost:9000 -DjobTracker=localhost:8032)
+JOB_ID=$(bin/oozie job -oozie http://localhost:11000/oozie -config examples/apps/shell/job.properties \
+-run -DnameNode=hdfs://localhost:9000 -DjobTracker=localhost:8032 | cut -d" " -f2)
 
